6035b99619f6c398e7f11ac41040454329be72f5
Syed Mir Habib <mirakik.habib@gmail.com>
2023-05-19 12:47:37 +0600

    Added Navbar from daisyUI

--- src/Layout/Main.jsx
+++ src/Layout/Main.jsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Footer from "../pages/shared/footer/Footer";
+import Navbar from "../pages/Shared/Navbar/Navbar";
 
 const Main = () => {
     return (
         <div>
+            <Navbar></Navbar>
             <Outlet></Outlet>
             <Footer></Footer>
         </div>
